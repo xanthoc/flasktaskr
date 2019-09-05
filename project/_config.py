681ec6153ee19efba11eb9d47aa337537506cdac
CJ Hwang <xanthoc68@gmail.com>
2019-09-06 05:41:17 +0900

    add _config for heroku

---
+++ project/_config.py
@@ -0,0 +1,16 @@
+import os
+
+# grab the foler where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_precisou'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = False
+
+#define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database URI
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
